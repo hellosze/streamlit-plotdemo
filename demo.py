9996de0f880e8ae921f55b45774013d443d3f2e5
sze chan <sze@hello-sze.com>
2024-10-19 16:14:28 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,11 +1,16 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 st.write('Hello world')
 
 penguin_df = pd.read_csv("penguins.csv")
 # print(penguin_df.head())
 st.dataframe(penguin_df)
+
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+
+st.line_chart(chart_data)
 
 
 uploaded_file = st.file_uploader("Choose a file")
