92be0c5af3a0cc9adb0dc9ff251b48f9d3192629
sze chan <sze@hello-sze.com>
2024-10-25 02:00:49 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -12,6 +12,14 @@ chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
 
 st.line_chart(chart_data)
 
+txt = st.text_area(
+    "Text to analyze",
+    "It was the best of times, it was the worst of times, it was the age of "
+    "wisdom, it was the age of foolishness, it was the epoch of belief, it "
+    "was the epoch of incredulity, it was the season of Light, it was the "
+    "season of Darkness, it was the spring of hope, it was the winter of "
+    "despair, (...)",
+)
 
 # uploaded_file = st.file_uploader("Choose a file")
 # if uploaded_file is not None:
